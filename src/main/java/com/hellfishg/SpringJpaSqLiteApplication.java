067a6d40a305a0ef8c65239cc5713340c30fdd8e
hellfish <hellfish.g@protonmail.com>
2019-05-10 17:15:29 -0300

    Plantilla del proyecto

---
+++ src/main/java/com/hellfishg/SpringJpaSqLiteApplication.java
@@ -0,0 +1,13 @@
+package com.hellfishg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSqLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaSqLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellfishg/SpringJpaSqLiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hellfishg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJpaSqLiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
